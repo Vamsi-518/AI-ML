c2363dd19783493f8f32b2c2a1bd044729fcbf0d
Vamsi-518 <chmnvkrishna@gmail.com>
2026-09-24 20:32:38 +0530

    add

---
+++ day12/student.py
@@ -0,0 +1,7 @@
+# student.py
+def student_details(name, age, course):
+    print("Student Name:", name)
+    print("Age:", age)
+    print("Course:", course)
+def add_marks(mark1, mark2):
+    return mark1 + mark2
