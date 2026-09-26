19532401755ea3b972c29bf234c483416ec5bb7f
Vamsi-518 <chmnvkrishna@gmail.com>
2026-09-26 19:40:13 +0530

    numpys

---
+++ day14/array_informations.py
@@ -0,0 +1,6 @@
+import numpy as np
+numbers = np.array([10, 20, 30, 40, 50])
+print("Size:", numbers.size)
+print("Dimensions:", numbers.ndim)
+print("Shape:", numbers.shape)
+print("Data type:", numbers.dtype)
---
+++ day14/array_operations.py
@@ -0,0 +1,6 @@
+import numpy as np
+numbers = np.array([10, 20, 30, 40, 50])
+print("Add 10:", numbers + 10)
+print("Subtract 5:", numbers - 5)
+print("Multiply by 2:", numbers * 2)
+print("Divide by 10:", numbers / 10)
